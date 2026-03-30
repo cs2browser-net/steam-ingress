ecbac6123ee0996ffb5f2cad6c4b360373c12cfe
skuzzis <andreieurt@gmail.com>
2026-03-30 18:56:07 +0300

    fix(main): Revert changes

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,24 @@ import type { Config } from "./types/config";
 import { serverCache } from './cache'
 import { db } from "./db/drizzle";
 import { server } from "../generated/drizzle/schema";
+import { requestServers } from "./servers/fetcher";
 
 config()
 
 const cfg: Config = JSON.parse(readFileSync("data/config.json").toString());
 
 const updateCache = async () => {
-    var start = Date.now();
     const servers = await db.select({ Address: server.address }).from(server);
 
     for (const row of servers) {
         serverCache.add(row.Address);
     }
-    var end = Date.now();
-    console.log(`Cache updated with ${servers.length} entries in ${(end - start) / 1000} seconds.`);
 }
 
 (async () => {
     await updateCache();
-    // await requestServers(cfg);
-    // setInterval(async () => {
-    //     await requestServers(cfg);
-    // }, 60000);
+    await requestServers(cfg);
+    setInterval(async () => {
+        await requestServers(cfg);
+    }, 60000);
 })()
